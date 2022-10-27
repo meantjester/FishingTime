ab75ff0236f8ce0046cc4374fadc82004c3fc8ac
meantjester <meantjester658@gmail.com>
2022-10-27 09:06:33 -0700

    Big spawn changes
    Thanks Mr. Lewis 10/10 code

--- FishinTime/Assets/Scripts/GameManager.cs
+++ FishinTime/Assets/Scripts/GameManager.cs
@@ -23,98 +23,82 @@ public class GameManager : MonoBehaviour
 
     public bool canspawnF;
     public bool canspawnS;
+
+    public int maxFish = 4;
+    public int fishWaveSpawns = 4;
+    public int maxShark = 3;
+    public int sharkWaveSpawns = 3;
+
     public float spawnrateF = 3.5f;
     public float spawnrateS = 5.5f;
+    public float fishCountdown = 0;
+    public float sharkCountdown = 0;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
+       
         
-        spawnFish = SpawnFish();
-
-        StartCoroutine(spawnFish);
-
-        spawnSharks = SpawnSharks();
-        
-    }
-
-    IEnumerator SpawnFish()
-    {
-        int i = 0;
-
-        //infinite loop
-        while (i <= 10)
-        {
-            canspawnF = true;
-
-            Vector3 randomPos = new Vector3(Random.Range(10f, 10f), Random.Range(-2.0f, -8.0f));
-            
-            //transfer enemy to 'EnemyContainer' gameobject.
-            GameObject newEnemy = Instantiate(FishPrefab, randomPos, Quaternion.identity);
-
-            i++;
-
-            //wait __ seconds 
-            yield return new WaitForSeconds(2f);
-
-            this.Wait(5.0f, () =>
-            {
-                StopCoroutine(spawnFish);
-                canspawnS = true;
-                StartCoroutine(spawnSharks);
-                Debug.Log("Bruh2");
-            });
-
-        }
-    }
-
-    IEnumerator SpawnSharks()
-    {
-        int i = 0;
-
-        //infinite loop
-        while (i <= 5)
-        {
-            canspawnS = true;
-
-            Vector3 randomPos = new Vector3(Random.Range(10f, 10f), Random.Range(-2.0f, -8.0f));
-            
-            i++;
-
-            //transfer enemy to 'EnemyContainer' gameobject.
-            GameObject newEnemy = Instantiate(SharkPrefab, randomPos, Quaternion.identity);
-
-            //wait __ seconds 
-            yield return new WaitForSeconds(5f);
-
-            this.Wait(15.0f, () =>
-            {
-                StopCoroutine(spawnSharks);
-                canspawnF = true;
-                StartCoroutine(spawnFish);
-                Debug.Log("Bruh");
-            });
-
-        }
     }
     
 
     // Update is called once per frame
     void Update()
     {
-        if (canspawnF)
+        if(canspawnF)
         {
-            canspawnS = false;
-            StopCoroutine(spawnSharks);
+            if(fishCountdown >= spawnrateF)
+            {
+                if (fishWaveSpawns <= 0)
+                {
+                    canspawnS = true;
+
+                    maxFish++;
+                    fishWaveSpawns = maxFish;
+
+                    canspawnF = false;
+                }
+
+                fishWaveSpawns--;
+                Vector3 randomPos = new Vector3(Random.Range(10f, 10f), Random.Range(-2.0f, -8.0f));
+
+                //transfer enemy to 'EnemyContainer' gameobject.
+                GameObject newEnemy = Instantiate(FishPrefab, randomPos, Quaternion.identity);
+                fishCountdown = 0;
+            }
+
+            else
+                fishCountdown += Time.deltaTime;
         }
 
         if (canspawnS)
         {
-            canspawnF = false;
-            StopCoroutine(spawnFish);
+            if (sharkCountdown >= spawnrateS)
+            {
+                if (sharkWaveSpawns <= 0)
+                {
+                    canspawnF = true;
+                    
+                    sharkWaveSpawns = maxShark;
 
+                    canspawnS = false;
+                }
+
+                sharkWaveSpawns--;
+                Vector3 randomPos = new Vector3(Random.Range(10f, 10f), Random.Range(-2.0f, -8.0f));
+
+                //transfer enemy to 'EnemyContainer' gameobject.
+                GameObject newEnemy = Instantiate(SharkPrefab, randomPos, Quaternion.identity);
+                sharkCountdown = 0;
+            }
+
+            else
+                sharkCountdown += Time.deltaTime;
         }
+
+      
     }
 
     public void startGame()
