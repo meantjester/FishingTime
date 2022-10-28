38ccc8f746b29d84c98391fd17fee11ca03a8936
meantjester <meantjester658@gmail.com>
2022-10-28 10:25:00 -0700

    ui

--- FishinTime/Assets/Scripts/MainMenu.cs
+++ FishinTime/Assets/Scripts/MainMenu.cs
@@ -4,11 +4,35 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-   public void PlayGame ()
+    public Text ScoreText;
+
+    public static MainMenu instance;
+
+    int score = 0;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void PlayGame ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void Start()
+    {
+        ScoreText.text = score.ToString() + "Score:";
+    }
+
+    public void AddPoint()
+    {
+        score += 1;
+        ScoreText.text = score.ToString() + "Score:";
     }
 }
--- FishinTime/Assets/Scripts/PlayerController.cs
+++ FishinTime/Assets/Scripts/PlayerController.cs
@@ -136,6 +136,8 @@ public class PlayerController : MonoBehaviour
 
             Destroy(gameObject);
 
+            MainMenu.instance.AddPoint();
+
         }
     }
 }
